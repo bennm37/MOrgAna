6c1a28b81b001ac3a7371a694cc594435ad81d22
Ben Mindlin <bennm37@gmail.com>
2024-10-11 15:15:20 +0100

    Evaluate

--- morgana/cli/evaluate.py
+++ morgana/cli/evaluate.py
@@ -1,14 +1,32 @@
 from morgana.MLModel.evaluate import evaluate
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_histograms(losses, classifier_accuracies, watershed_accuracies):
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    bins = np.linspace(0, 1, 30)
+    ax[0].hist(losses, bins=bins)
+    ax[0].set_title("Losses")
+    ax[1].hist(classifier_accuracies, bins=bins)
+    ax[1].set_title("Classifier Accuracies")
+    ax[2].hist(watershed_accuracies, bins=bins)
+    ax[2].set_title("Watershed Accuracies")
+    plt.show()
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("model_folder", type=str)
+    p.add_argument("-p", "--plot", action="store_true", default=False)
     args = p.parse_args()
-    evaluate(args.model_folder)
+    l, ca, wa = evaluate(args.model_folder)
+    if args.plot:
+        plot_histograms(l, ca, wa)
 
 
 if __name__ == "__main__":
     model_folder = "/Users/nicholb/Documents/data/organoid_data/fullModel"
-    evaluate(model_folder)
+    losses, classifier_accuracies, watershed_accuracies = evaluate(model_folder)
+    plot_histograms(losses, classifier_accuracies, watershed_accuracies)
